e880b693b0f947e7ea0bc58d2f4f8605575a4abc
BigJoon <hiyoungih@naver.com>
2021-01-13 12:55:50 +0900

    Update teleporter.py

--- Script_Collection/Object_Detection_RealTime_inference/teleporter.py
+++ Script_Collection/Object_Detection_RealTime_inference/teleporter.py
@@ -10,13 +10,6 @@ import shutil
 dataset_path = '/hdd/TMP_D/Resized_Data_Voc_For_Song/VOCdevkit/VOC2007'
 image_dir_path = os.path.join(dataset_path,'JPEGImages')
 annotations_dir_path = os.path.join(dataset_path, 'Annotations')
-#trainval_txt_path = os.path.join(dataset_path, 'ImageSets/Main/trainval.txt')
-#test_txt_path = os.path.join(dataset_path, 'ImageSets/Main/test.txt')
-
-#TODO : trainval.txt 읽어와서 해당하는 파일들을 각각 Annotations 과 JPEGImages 에서 빼서
-# /hdd/Fresh_Data/Class6/VOCdevkit/VOC2007/JPEGImages
-# /hdd/Fresh_Data/Class6/VOCdevkit/VOC2007/Annotations
-# 위 경로로 "복사"해야함.
 
 ##First Read "trainval.txt"
 f = open('trainval.txt',mode='rt',encoding='utf-8')
@@ -25,12 +18,7 @@ line_num=1
 line = f.readline()
 
 while line:
-    #print('%d %s' %(line_num,line))
-    #print(line)  ==> line에 파일들 잘 들어가 잇음.
-    #TODO 여기서 line 에 들어가 있는 파일이름으로 복사하는거 하면 됨. Annotation 이랑 JPEGImages로!!!
-    
-    #line.rstrip('\n')
-    #print(line.rstrip('\n'))
+
     src = '/hdd/TMP_D/Resized_Data_Voc_For_Song/VOCdevkit/VOC2007/Annotations/'+line.rstrip('\n')+'.xml'
     #print(src)
     dst = '/hdd/Fresh_Data/Class8_A/VOCdevkit/VOC2007/Annotations/'
@@ -39,5 +27,3 @@ while line:
     line_num +=1
 
 f.close()
-
-#print(f.readline())
